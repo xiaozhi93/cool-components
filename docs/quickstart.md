ae39d66c0cb605cbbedf9ff23e1f515a8c753629
jzy01309237 <jzy01309237@alibaba-inc.com>
2025-07-01 11:08:15 +0800

    feat: 添加项目基础结构，包括 ESLint 和 Git 忽略文件，初始化 package.json 和 pnpm 锁定文件，配置 VantCli，创建 DemoButton 组件及其文档和测试用例。

---
+++ docs/quickstart.md
@@ -0,0 +1,17 @@
+# 快速上手
+
+### 安装
+
+```bash
+# 通过 npm
+npm i cool-components
+
+# 通过 yarn
+yarn add cool-components
+
+# 通过 pnpm
+pnpm add cool-components
+
+# 通过 bun
+bun add cool-components
+```
---
+++ vant.config.mjs
@@ -0,0 +1,41 @@
+export default {
+  name: 'cool-components',
+  build: {
+    css: {
+      preprocessor: 'sass',
+    },
+    site: {
+      publicPath: '/cool-components/',
+    },
+    packageManager: 'pnpm',
+  },
+  site: {
+    title: 'cool-components',
+    logo: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
+    nav: [
+      {
+        title: '开发指南',
+        items: [
+          {
+            path: 'home',
+            title: '介绍',
+          },
+          {
+            path: 'quickstart',
+            title: '快速上手',
+          },
+        ],
+      },
+      {
+        title: '基础组件',
+        items: [
+          {
+            path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+    ],
+    hideSimulator: true,
+  },
+};
